2021b027dde84808608977c510613e8709479f8f
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-08-27 14:40:04 +0530

    Time: 438 ms (66.3%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 1683-article-views-i/1683-article-views-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT DISTINCT AUTHOR_ID AS ID FROM VIEWS WHERE AUTHOR_ID = VIEWER_ID
+ORDER BY AUTHOR_ID;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
