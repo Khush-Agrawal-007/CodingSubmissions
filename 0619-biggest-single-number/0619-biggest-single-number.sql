02bf4f31aafdf1d4f1f68b1c0363b11f5348ef7d
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-08-30 14:32:04 +0530

    Time: 525 ms (28.49%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,12 @@
 # Write your MySQL query statement below
-select COalesce ((select num from MyNumbers
+
+select max(num) num from (select 
+num
+from 
+    mynumbers
 group by num
-having count(num)<=1 
-order by num desc
-limit 1),null) as num
-;
-/*-- i want to find max of nums where it is occured only one time
-*/
+having count(num)<=1) as single_nums
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
